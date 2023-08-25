78c4fe75ff7f61a08e7c8c47d2c3245bccdd3161
Oliverholm <oliverxholmberg@gmail.com>
2023-08-25 11:41:23 +0200

    Conflicts solved

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import "./App.css";
-<<<<<<< HEAD
 import NavBar from "./components/navbar";
-
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-    </div>
-  );
-}
-
-
-=======
 import { PostList } from "./components/PostList";
 
 function App() {
 	return (
 		<div className="App">
+      <NavBar />
 			<PostList />
 		</div>
 	);
 }
 
->>>>>>> ad63549a63e5a060cf31aedf4bd53fc95ca1e944
 export default App;
