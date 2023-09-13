561488f54f10a5d417d2f9dcf8a9b977908b1994
AlptegJ <127868938+AlptegJ@users.noreply.github.com>
2023-09-13 15:17:44 +0200

    minor stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { useState } from "react";
 import { CreatePostComponent } from "./components/createpost";
+import { PostList } from "./components/postlist";
 
 function App() {
   return (
     <div className="App">
       <CreatePostComponent />
+      <PostList />
     </div>
   );
 }
