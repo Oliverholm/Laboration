66f6fa9c522ddb57913bcba5caa54b37f9cf8701
Oliverholm <oliverxholmberg@gmail.com>
2023-09-18 20:16:18 +0200

    Nav style/func
    Small updates

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,26 +1,36 @@
-import { Meh, MoreVertical } from "react-feather"
-import '../styles/navbar.css' 
+import { Meh, MoreVertical, Command } from "react-feather";
+import "../styles/navbar.css";
 
+export function NavBar() {
+  const clickHandler = () => {};
+  return (
+    <div className="nav-flex">
+      <div className="nav-flex">
+        <Meh size={"80px"} color="white" />
+        <h1 className="logo-nav mt">psq</h1>
+      </div>
+      <span className="flex">
+        <input
+          className="nav-searchbar "
+          type="search"
+          placeholder="Search Our Fake Reddit"
+        />  
+        <button onClick="" className="btn-icon">
+          <Search className="icon" />
+        </button>
+      </span>
 
-function NavBar() {
-    return (
-        <div className="nav-flex">
-            <div className="nav-flex">
-                <Meh size={"80px"} color="white"/>
-                <h1 className="logo-nav">psq</h1>
-            </div>
-            <div>
-                <input type="search" placeholder="Search Our Fake Reddit" />
-                <button>Icon</button>
-            </div>
-            <ul>
-                <li></li>
-                <li> Icon</li>
-                <li> Icon</li>
-            </ul>
-            
-        </div>
-    )
+      <span className="flex">
+        <span>
+          <button className="btn">
+            <Command size={"20px"} />
+            Get App
+          </button>
+        </span>
+        <button className="btn">Sign In</button>
+      </span>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
